f50a68d9ab7c4480963eebb71c4df9b4d42ca70d
Jesse Rosalia <jesse.rosalia@gatech.edu>
2013-08-15 13:35:48 -0400

    Wired up OpenCV for Android, and tested it by adding inRange call Build/run instructions

---
+++ Dojo5/README.md
@@ -0,0 +1,44 @@
+BlobifyMe
+==========
+
+This project demonstrates the use of OpenCV on Android to perform simple blob detection on a captured image.  It utilizes the built in Android Camera app to capture the picture.
+
+## Dependencies
+
+This project depends on the OpenCV for Android library, located at (http://sourceforge.net/projects/opencvlibrary/files/opencv-android/).  Make sure you download the library for Android; the "latest version" link will take you to the full OpenCV package, which will not work.
+
+## Building[^1]
+
+This guide assumes that you have Eclipse (tested on Indigo SR2) and Android SDK and ADT installed and configured.  OpenCV requires Android SDK 11 (Honeycomb), and BlobifyMe requires Android SDK 17 (Jelly Bean 4.2.2)
+
+### Instructions:
+
+1. Download the OpenCV for Android library, and extract on your local machine.  For the purposes of this guide, we'll use the folder ~/OpenCV4Android.
+
+2. Import this project into Eclipse using *File -> Import -> Existing Projects into Workspace*
+
+3. Clone the Dojo code onto your machine:  
+```
+cd ~/Development
+https://github.com/pfunk-ml/Dojo.git
+```
+
+4. Import Dojo5/blobifyme into Eclipse using *File -> Import -> Existing Projects into Workspace*
+
+5. Open the Android project properties for blobifyme by right clicking on the project, selecting *Properties*, and then clicking *Android* in the list on the left.
+
+6. Add OpenCV as a referenced library by clicking the *Add* button near the bottom of the page, and selecting the OpenCV Library project.
+
+## Running
+
+The first time this is run, it will check if OpenCV Manager is installed.  If not, it will ask you if you want it to find OpenCV Manager, which will direct you to the Google Play store.  Complete the installation on Google Play, and then relaunch BlobifyMe.  You can also hit the back button to return to the currently launched BlobifyMe app.  It may ask you again to install OpenCV Manager, but you can press *No* in this case.
+
+### To Operate
+
+1. Press the *Take Picture* button to launch the camera interface
+2. Take a picture, and click the check mark to accept it
+3. TBD
+
+
+[^1]: Build instructions adopted from (http://docs.opencv.org/doc/tutorials/introduction/android_binary_package/dev_with_OCV_on_Android.html)
+
--- Dojo5/blobifyme/src/edu/gatech/cc/pfunk/blobifyme/BlobifyMeApp.java
+++ Dojo5/blobifyme/src/edu/gatech/cc/pfunk/blobifyme/BlobifyMeApp.java
@@ -1,10 +1,24 @@
 package edu.gatech.cc.pfunk.blobifyme;
 
+import java.io.InputStream;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,12 +27,31 @@ import android.widget.ImageView;
 
 public class BlobifyMeApp extends Activity {
 
+    private static final String TAG = BlobifyMeApp.class.getSimpleName();
     private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
     private ImageView pictureView;
     private Button    takePictureBtn;
 
     private Uri fileUri;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+//                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +68,14 @@ public class BlobifyMeApp extends Activity {
         });
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_6, this, mLoaderCallback);
+    }
+
+    
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -59,6 +100,20 @@ public class BlobifyMeApp extends Activity {
         //only set the pictureview if the image capture was successful
         if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE && resultCode != RESULT_CANCELED) {
             pictureView.setImageURI(this.fileUri);
+            try {
+                InputStream is = getContentResolver().openInputStream(this.fileUri);
+                Bitmap bitmap = BitmapFactory.decodeStream(is);
+                Mat mRgba = new Mat();
+                Utils.bitmapToMat(bitmap, mRgba);
+                Mat HSV = new Mat();
+                
+                Imgproc.cvtColor(mRgba, HSV, Imgproc.COLOR_RGB2HSV, 3);
+                Core.inRange(HSV,  new Scalar(0, 100, 30), new Scalar(5, 255, 255), mRgba);
+                System.out.println(HSV);
+//                Imgproc.cvtColor(mRgba, mRgba, Imgproc.COLOR_GRAY2BGRA, 4); 
+            } catch (Exception e) {
+                
+            }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
